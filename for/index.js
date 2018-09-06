457b5f9892483b098f73a1e282792fec1dea17f1
turtle0617 <qqaz50313@gmail.com>
2018-09-06 15:39:55 +0800

    for and transition finish

---
+++ for/index.js
@@ -0,0 +1,12 @@
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos:{}
+  }
+})
+// console.log(app4.todos);
+app4.todos= [
+  { text: 'Learn JavaScript' },
+  { text: 'Learn Vue' },
+  { text: 'Build something awesome' }
+]
