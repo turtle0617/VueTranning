3473da4a1be8f60cf8e59b41b602f1ab58d24fd9
turtle0617 <qqaz50313@gmail.com>
2018-09-07 09:36:47 +0800

    todo start!

---
+++ component/index.js
@@ -0,0 +1,22 @@
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
---
+++ todolist/index.js
@@ -0,0 +1,6 @@
+new Vue({
+  el:'.todoapp',
+  data:{
+    message:'hello world'
+  }
+})
