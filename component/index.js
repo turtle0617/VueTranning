566b0228b5a4c32465bb4cf6f7b1d575c0beaacd
turtle0617 <qqaz50313@gmail.com>
2018-09-08 16:36:35 +0800

    v1.0

--- component/index.js
+++ component/index.js
@@ -1,3 +1,4 @@
+
 Vue.component('todo-item', {
   props: ['todo'],
   template: '<li>{{ todo.text }}</li>'
@@ -10,6 +11,18 @@ Vue.component('button-counter', {
   },
   template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
 })
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text', 0.1)">
+        Enlarge text
+      </button>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
 var app7 = new Vue({
   el: '#app-7',
   data: {
@@ -17,6 +30,18 @@ var app7 = new Vue({
       { id: 0, text: 'Vegetables' },
       { id: 1, text: 'Cheese' },
       { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
+    ],
+    posts: [
+     { id: 1, title: 'My journey with Vue' },
+     { id: 2, title: 'Blogging with Vue' },
+     { id: 3, title: 'Why Vue is so fun' }
+   ],
+    postFontSize: 1
+ },
+ methods: {
+  onEnlargeText: function (enlargeAmount) {
+    console.log(enlargeAmount);
+    this.postFontSize += enlargeAmount
   }
+}
 })
--- todolist/index.js
+++ todolist/index.js
@@ -1,6 +1,34 @@
 new Vue({
-  el:'.todoapp',
-  data:{
-    message:'hello world'
+  el: '.todoapp',
+  data: {
+    newTask: '',
+    tasks: [],
+  },
+  methods: {
+    addTask: function() {
+      if (this.newTask.length === 0) {
+        alert("請勿輸入空白");
+        return
+      }
+      this.tasks.push({
+        id: this.tasks.length,
+        title: this.newTask,
+        complete: false,
+        input_Status: true,
+        label_Status: false,
+        isComplete: false
+      })
+      this.newTask = ""
+    },
+    deleteTask: function(task) {
+      this.tasks.splice(this.tasks.indexOf(task), 1);
+    },
+    changeStatus: function(task) {
+      task.input_Status = !task.input_Status;
+      task.label_Status = !task.label_Status;
+    },
+    complete: function(task) {
+      task.isComplete = !task.isComplete;
+    }
   }
-})
+});
